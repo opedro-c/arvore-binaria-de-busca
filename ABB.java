1cbe028396c6c5fd244662a026461bcede514413
Esther <esther.wanderley@uol.com.br>
2022-10-27 22:16:41 -0300

    Done pre-order and printing

--- ABB.java
+++ ABB.java
@@ -1,9 +1,12 @@
+import java.util.Stack;
+
 public class ABB {
     //Atributos (precisa de mais??)
     private int altura;
     private ABB subarvoreEsquerda;
     private ABB subarvoreDireita;
     private int valor;
+    //private int numTracos; //p/ impressao 1
 
     //Métodos default:
     public ABB(){
@@ -11,12 +14,14 @@ public class ABB {
         subarvoreEsquerda = null;
         subarvoreDireita = null;
         valor = 0;
+        //numTracos = 30;
     }
     public ABB(int valor){
         altura = 0;
         subarvoreEsquerda = null;
         subarvoreDireita = null;
         this.valor = valor;
+        //numTracos = 30;
     }
     public void insert(int x){
         if(x<valor){
@@ -61,13 +66,58 @@ public class ABB {
         return false;
     }
     public String pre_ordem(){
-        return "Gaiata";
+        Stack values = new Stack<ABB>();
+        String order="";
+        values.push(this);
+        while(!values.empty()){
+            ABB top = (ABB) values.pop();
+            System.out.println(top.valor);
+            if(top.subarvoreDireita != null){
+                values.push(top.subarvoreDireita);
+            }
+            if(top.subarvoreEsquerda != null){
+                values.push(top.subarvoreEsquerda);
+            }
+        }
+        while (!values.empty()) {
+            order+=((ABB)(values.peek())).valor;
+            order+=" ";
+            values.pop();
+        }
+        return order;
     }
     public void imprimeArvore(int s){
         if(s==1){
-
+            imprimeArvore1();
+            //numTracos = 30;
         } else if (s==2) {
-
+            System.out.println(stringArvore2());
         } else System.out.println("Desculpe, só existem dois tipos de impressão.\nEntre com '1' ou '2'.");
     }
+    private void imprimeArvore1(){
+        for (int i=0; i<this.altura*5; ++i) System.out.print(' ');
+        System.out.print(valor);
+        for (int i=0; i<30-this.altura*5; ++i) System.out.print('-');
+        System.out.println();
+        if(subarvoreEsquerda != null){
+            subarvoreEsquerda.imprimeArvore1();
+        }
+        if(subarvoreDireita != null){
+            subarvoreDireita.imprimeArvore1();
+        }
+    }
+    private String stringArvore2(){
+        String fullString = " (";
+        fullString += valor;
+        //fullString += " ";
+        if(subarvoreEsquerda != null){
+            fullString += subarvoreEsquerda.stringArvore2();
+        }
+        if(subarvoreDireita != null){
+            fullString += subarvoreDireita.stringArvore2();
+        }
+        fullString += ")";
+        return fullString;
+    }
+
 }
